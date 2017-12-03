d7e2f72631925a8eb751148082d0f7c03f3bd696
marcov <marco@marcov.local>
2017-12-03 19:46:14 +0100

    Update very basic makefile

--- Makefile_basic.mk
+++ Makefile_basic.mk
@@ -3,16 +3,24 @@
 #                         (C) Marco Vedovati, 2016                              #
 #################################################################################
 
-PROJECT_NAME = $(shell basename ${PWD})
+# For help and usage, run "make help"
 
-#################################################################################
+PROJECT_NAME := $(shell basename ${PWD})
+
+# Specify a cross-compiler prefix if needed
+CROSS_PREFIX :=
+
+################################################################################
 # Note: VPATH is a standard Makefile variable,
 # specifying a list of directories that make should search.
-VPATH = .
+ifndef VPATH
+  $(info Attention: no VPATH variable defined specifying all the locations of the source code. Using "." as the only default location)
+  VPATH := .
+endif
 
-
-CFLAGS_WARN   = -Wall -Werror -Wextra -Wundef 
-CFLAGS        = $(CFLAGS_WARN) -c -g -ggdb 
+CC := $(CROSS_PREFIX)gcc
+CFLAGS_WARN   = -Wall -Werror -Wextra -Wundef
+CFLAGS        = $(CFLAGS_WARN) -c -g -ggdb
 AT            = @
 BUILD_OBJ_DIR = build
 
@@ -30,11 +38,20 @@ CFLAGS  += $(foreach dir,$(INCDIRS),-I$(dir) )
 # Define the C source files by searching in each VPATH element.
 C_SRCS  := $(foreach dir,$(VPATH),$(wildcard $(dir)/*.c))
 
+# Include single files from the files lis as well.
+C_SRCS += $(C_SRC_FILESLIST)
+
+# We need to include the directories of single files to VPATH to make pattern rules work...
+VPATH += $(dir $(C_SRC_FILESLIST))
 
 # Define the object files
 OBJS := $(addprefix $(BUILD_OBJ_DIR)/,$(notdir $(C_SRCS:.c=.o)))
 
 
+#################################################################################
+#
+# Rules section
+#
 
 .PHONY: obj_compile
 obj_compile:
@@ -51,7 +68,13 @@ $(BUILD_OBJ_DIR)/%.o: %.c
 	$(CC) $(CFLAGS) -o $@ $< || exit $$?;     \
 
 
-$(BUILD_OBJ_DIR)/$(PROJECT_NAME).bin: obj_compile
+$(BUILD_OBJ_DIR)/$(PROJECT_NAME): $(BUILD_OBJ_DIR)/$(PROJECT_NAME).elf
+	$(AT)                                              \
+	echo "~ Stripping $@          ";                   \
+	$(STRIP_CMD) $< $(STRIP_OPTS) $@  || exit $$?;     \
+
+
+$(BUILD_OBJ_DIR)/$(PROJECT_NAME).elf: obj_compile
 	$(AT)                                      \
 	echo "x Linking $@ ";                      \
 	$(CC) $(LFLAGS) -o $@ $(OBJS) || exit $$?; \
@@ -63,12 +86,42 @@ $(BUILD_OBJ_DIR)/$(PROJECT_NAME).bin: obj_compile
 default: all
 
 .PHONY: all
-all: $(BUILD_OBJ_DIR)/$(PROJECT_NAME).bin
-
+all: $(BUILD_OBJ_DIR)/$(PROJECT_NAME)
+	$(AT) \
+	echo "Done.\n" ; \
 
 .PHONY: clean
 clean:
 	$(AT)                                      \
 	echo "Deleting build artifacts...";        \
 	rm -rf $(BUILD_OBJ_DIR) || exit $$?;       \
+
+.PHONY: help
+help:
+	$(AT) \
+	echo "\n   Very basic Makefile \n \
+	\n \
+	TL;DR: For a typical usage, clone the git repo containing this Makefile into your main project directory. \n \
+	Then, create a local Makefile with the following content: \n \
+	\n \
+	##### --- Cut from here --- #### \n \
+	PROJECT_NAME := my_cool_project_name  \n \
+	\n \
+	# Build all files inside the following directories list: \n \
+	VPATH := dir1 dir2 dir3  \n \
+	\n \
+	C_SRC_FILESLIST := folder1/single_file_to_build.c folder2/another_single_file_to_build.c \n \
+	\n \
+	-include Makefiles/Makefile_w_deps.mk \n \
+	##### --- Cut till here --- #### \n \
+	\n \
+	~~~~~~~\n \
+	This is the list of make variables you should define or could override: \n \
+	\n \
+	- VPATH: this is the list of directories containing source code files. NOTE: All files inside these directories will be built! \n \
+	  To specify single files, use the variable C_SRC_FILESLIST. \n \
+	  These directories are used as well as 'include' directories for your compiler. \n \
+	\n \
+	- C_SRC_FILESLIST: this is a list of single files that should be built. \n \
+	  NOTE: If you want to build all files in a directory, use 'VPATH' instead." \
 
